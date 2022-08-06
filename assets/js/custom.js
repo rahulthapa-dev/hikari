289e9b385ff0cad75856b79eb1d9ff3c4eca6772
rahulthapa-dev <rahul.thapa91@gmail.com>
2022-08-06 20:36:37 +0530

    blog and blog list

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,4 +22,19 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 // e.init();
+// $("#header").headroom();
+// headroom.init();
 AOS.init();
+
+// O = function () {
+//   var m = $('#arrow-top-c');
+//   var e = m.parent().width(),
+//     t = e / 2,
+//     n = 3;
+//   m.parent().attr("viewBox", "0 0 ".concat(e, " ").concat(e)), m.attr("stroke-width", n), m.attr("r", t - (n - 1)), m.attr("cx", t), m.attr("cy", t), g = 2 * t * Math.PI, m[0].style.strokeDasharray = "".concat(g, " ").concat(g), m[0].style.strokeDashoffset = g
+// }
+
+// O();
+
+
+
